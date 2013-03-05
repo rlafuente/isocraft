2d2cbd47b71beb679f38677bfec7ce685ced3f0a
Ricardo Lafuente <bollecs@sollec.org>
2013-03-05 08:19:26 -0800

    More block definitions

--- blockdefs.py
+++ blockdefs.py
@@ -5,44 +5,45 @@ BLOCKDEFS = {
   3: color(116, 81, 55),    # dirt
   4: color(165, 165, 165),  # cobblestone
   5: color(178, 144, 92),   # oak planks
-  9: color(51, 75, 255),
-  12: color(210, 204, 148),
-  17: color(138, 112, 71),
-  18: color(164, 164, 164),
-  20: color(170, 182, 184),
-  25: color(140, 86, 69),
-  26: color(177, 57, 53),
-  27: color(201, 135, 28),
-  28: color(137, 117, 88),
-  31: color(64, 64, 64), # não existe
-  33: color(64, 64, 64), # não existe
-  35: color(220, 140, 162), # lã, múltiplas cores
-  37: color(234, 242, 2),
-  38: color(247, 4, 15),
-  43: color(172, 172, 172),
-  44: color(165, 165, 165),
-  47: color(172, 139, 89),
-  50: color(255, 143, 0),
-  53: color(186, 151, 97),
-  56: color(120, 189, 225),
-  61: color(137, 137, 137),
-  64: color(150, 120, 61),
-  67: color(154, 154, 154),
-  68: color(168, 137, 82),
-  69: color(142, 112, 67),
+  8: color(51, 75, 255),    # water  
+  9: color(51, 75, 255),    # stationary water
+  12: color(210, 204, 148), # sand
+  17: color(138, 112, 71),  # wood (v.ref)
+  18: color(164, 164, 164), # leaves (v.ref)
+  20: color(170, 182, 184), # glass
+  25: color(140, 86, 69),   # note block
+  26: color(177, 57, 53),   # bed
+  27: color(201, 135, 28),  # powered rail
+  28: color(137, 117, 88),  # detector rail
+  31: color(64, 64, 64),    # grass
+  33: color(64, 64, 64),    # piston
+  35: color(220, 140, 162), # wool (v.ref)
+  37: color(234, 242, 2),   # dandelion
+  38: color(247, 4, 15),    # rose
+  43: color(172, 172, 172), # double slabs (v.ref)
+  44: color(165, 165, 165), # slabs (v.ref)
+  47: color(172, 139, 89),  # bookshelf
+  50: color(255, 143, 0),   # torch
+  53: color(186, 151, 97),  # oak wood stairs
+  56: color(120, 189, 225), # diamond ore
+  61: color(137, 137, 137), # furnace
+  64: color(150, 120, 61),  # wooden door
+  67: color(154, 154, 154), # cobblestone stairs
+  68: color(168, 137, 82),  # wall sign
+  69: color(142, 112, 67),  # lever
   70: color(137, 137, 137), # stone pressure plate
   71: color(205, 205, 205), # iron door
-  72: color(169, 139, 85), # wooden pressure plate
-  76: color(237, 94, 19), # redstone torch (active)
+  72: color(169, 139, 85),  # wooden pressure plate
+  76: color(237, 94, 19),   # redstone torch (active)
   79: color(133, 180, 255), # ice
   85: color(170, 146, 109), # fence
   89: color(211, 166, 101), # glowstone
-  96: color(64, 64, 64), # trapdoor
-  102: color(193, 224, 229), # glass pane
-  126: color(64, 64, 64), # wooden slab
+  96: color(64, 64, 64),    # trapdoor
+  102: color(193, 224, 229),# glass pane
+  126: color(64, 64, 64),   # wooden slab
   131: color(162, 132, 82),	# tripwire hook
-  134: color(64, 64, 64), # spruce wood stairs
-  139: color(64, 64, 64), # cobblestone wall
-  140: color(125, 73, 58), # flower pot
-  143: color(64, 64, 64), # não existe
+  134: color(64, 64, 64),   # spruce wood stairs
+  139: color(64, 64, 64),   # cobblestone wall
+  140: color(125, 73, 58),  # flower pot
+  143: color(64, 64, 64),   # wooden button
   }
