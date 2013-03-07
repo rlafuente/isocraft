8235d52732aa6f46f74222fcc06cbad75b7a700d
Ricardo Lafuente <r@sollec.org>
2013-03-07 22:56:06 +0000

    New block colors, plus blocks with data! Also argparse!

--- blockdefs.py
+++ blockdefs.py
@@ -9,25 +9,41 @@ BLOCKDEFS = {
   2: (116, 81, 55),    # dirt with grass      
   3: (116, 81, 55),    # dirt
   4: (165, 165, 165),  # cobblestone
-  5: (178, 144, 92),   # oak planks
+  5: (178, 144, 92),   # wood planks (see data)
   8: (51, 75, 255),    # water  
   9: (51, 75, 255),    # stationary water
   12: (210, 204, 148), # sand
-  17: (138, 112, 71),  # wood (v.ref)
-  18: (164, 164, 164), # leaves (v.ref)
+  17: (138, 112, 71),  # wood (see data)
+  18: (164, 164, 164), # leaves (see data)
   20: (170, 182, 184), # glass
-  24: (210, 204, 148), # sandstone
+  24: (210, 204, 148), # sandstone (see data)
   25: (140, 86, 69),   # note block
   26: (177, 57, 53),   # bed
   27: (201, 135, 28),  # powered rail
   28: (137, 117, 88),  # detector rail
-  31: (64, 64, 64),    # grass
-  33: (64, 64, 64),    # piston
-  35: (220, 140, 162), # wool (v.ref)
+  31: (99, 150, 85),   # grass
+  33: (178, 144, 92),  # piston
+  35: {                # wool
+       0: (222, 222, 222), # white
+       1: (221, 132, 74),  # orange
+       2: (182, 80, 190),  # violet
+       3: (104, 136, 200), # light blue
+       4: (198, 185, 46),  # yellow
+       5: (64, 172, 55),   # lime
+       6: (208, 134, 153), # pink
+       7: (66, 66, 66),    # dark grey
+       8: (162, 168, 168), # light grey
+       9: (47, 112, 139),  # teal
+       10: (122, 58, 178), # purple
+       11: (43, 53, 133),  # dark blue
+       12: (87, 55, 34),   # brown
+       13: (55, 73, 28),   # green
+       14: (152, 53, 50),  # red
+       15: (28, 25, 25)},  # black
   37: (234, 242, 2),   # dandelion
   38: (247, 4, 15),    # rose
-  43: (172, 172, 172), # double slabs (v.ref)
-  44: (165, 165, 165), # slabs (v.ref)
+  43: (172, 172, 172), # double slabs (see data)
+  44: (165, 165, 165), # slabs (see data)
   47: (172, 139, 89),  # bookshelf
   49: (50, 50, 50),    # obsidian
   50: (255, 143, 0),   # torch
@@ -43,16 +59,18 @@ BLOCKDEFS = {
   72: (169, 139, 85),  # wooden pressure plate
   76: (237, 94, 19),   # redstone torch (active)
   79: (133, 180, 255), # ice
-  82: (0,0,0),         # clay
+  82: (119, 169, 255), # clay
   85: (170, 146, 109), # fence
-  88: (0,0,0),         # soul sand
+  88: (91, 71, 58),    # soul sand
   89: (211, 166, 101), # glowstone
-  96: (64, 64, 64),    # trapdoor
+  96: (178, 144, 92),  # trapdoor
   102: (193, 224, 229),# glass pane
-  126: (64, 64, 64),   # wooden slab
+  126: (178, 144, 92), # wooden slab
   131: (162, 132, 82), # tripwire hook
-  134: (64, 64, 64),   # spruce wood stairs
-  139: (64, 64, 64),   # cobblestone wall
+  134: (178, 144, 92), # spruce wood stairs
+  139: (165, 165, 165),# cobblestone wall
   140: (125, 73, 58),  # flower pot
-  143: (64, 64, 64),   # wooden button
+  143: (178, 144, 92), # wooden button
   }
+
+BLOCKS_WITH_DATA = [k for k in BLOCKDEFS if type(BLOCKDEFS[k]) == dict]
--- isocraft.py
+++ isocraft.py
@@ -1,52 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
+import sys, os
 import numpy as np
 from nbt import nbt
 import shoebot
 
-from blockdefs import BLOCKDEFS
+from blockdefs import BLOCKDEFS, BLOCKS_WITH_DATA
 
-# change these!
-
-INPUT_FILE = sys.argv[1]
-bot = shoebot.sbot.init_bot(outputfile="output.svg") 
-
-BLOCK_SIZE = 2
-ORIGIN_X = 150
-ORIGIN_Z = 100
-BRIGHTNESS_STEP = 0.05
-padding = 1
-rotation = 0
-
-bot.colormode(bot.RGB)
-bot.colorrange(255)
-
-bs = BLOCK_SIZE
-
-def draw_block(x,y, blocktype):
+def draw_block(x, y, blocktype, blockdata, bot):
     '''
     draw an isometric square prism; x,y are the coordinates of
     the BOTTOM-CENTER corner, z is the height
     '''
     
     if blocktype in BLOCKDEFS:
-        blockcolor = bot.color(*BLOCKDEFS[blocktype])
+        if blocktype in BLOCKS_WITH_DATA:
+            blockcolor = bot.color(*BLOCKDEFS[blocktype][blockdata])
+        else:
+            blockcolor = bot.color(*BLOCKDEFS[blocktype])
     else:
-        blockcolor = bot.color(64, 64, 64)
+        blockcolor = bot.color(50, 0, 0)
     
-    # FIXME: z and y should be switched around
-    z = bs * 2    
+    leftcolor = bot.color(blockcolor)
+    leftcolor.brightness -= BRIGHTNESS_STEP
+    rightcolor = bot.color(blockcolor)
+    rightcolor.brightness -= BRIGHTNESS_STEP * 2
+    topcolor = bot.color(blockcolor)
 
-    # no strokes in the inside
+    # FIXME: z and y should be switched around
+    z = bs * 2
+
     bot.nostroke()
 
     # LEFT FACE
-    # first, set the color
-    c = bot.color(blockcolor)
-    c.brightness -= BRIGHTNESS_STEP
-    bot.fill(c)
-    # and draw the path
+    bot.fill(leftcolor)
     bot.beginpath(x, y)
     bot.lineto(x-bs*2, y-bs)
     bot.lineto(x-bs*2, y-bs-z)
@@ -54,10 +41,7 @@ def draw_block(x,y, blocktype):
     bot.endpath()
 
     # RIGHT FACE
-    # darker
-    c = bot.color(blockcolor)
-    c.brightness -= BRIGHTNESS_STEP * 2
-    bot.fill(c)
+    bot.fill(rightcolor)
     bot.beginpath(x, y)
     bot.lineto(x+bs*2, y-bs)
     bot.lineto(x+bs*2, y-bs-z)
@@ -65,49 +49,66 @@ def draw_block(x,y, blocktype):
     bot.endpath()
 
     # TOP FACE
-    c = bot.color(blockcolor)
-    bot.fill(c)
+    bot.fill(topcolor)
     bot.beginpath(x, y-z)
     bot.lineto(x+bs*2, y-bs-z)
     bot.lineto(x, y-bs*2-z)
     bot.lineto(x-bs*2, y-bs-z)
     bot.endpath()
 
-def parse_line(line):
-    coordtuple, blocktype = line.split(':')
-    x, y, z = coordtuple.strip('()').split(',')
-    x = int(x)
-    y = int(y)
-    z = int(z)
-    blocktype = int(blocktype.strip())
-    return x, y, z, blocktype
-
 def get_block_from_nbt(x, y, z, blocks):
     # http://www.minecraftforum.net/topic/16084-baezons-redstone-simulator-v22/page__st__40#entry364309
     # this formula saved my day!
-    # i changed it to correspond to YZX order, according to NBT/Anvil filespec
     return blocks[x + z*width + y*length*width]
 
+def remove_invisible_blocks(max_x, max_y, max_z, matrix):
+    ray_matrix = np.empty((max_x+1, max_y+1, max_z+1))
+    for x in width:
+        for y in height:
+            ray_matrix[x,y,0] = 5 # wood block
+
+    
+
+
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("inputfile", help="schematic filename")
+    parser.add_argument("-o", "--outputfile", help="output filename (possible extensions are .pdf, .png, .svg and .ps)", dest="outputfile", metavar="FILE")
+    parser.add_argument("-v", "--verbose", help="print extra information to terminal", action="store_true")
+    parser.add_argument("-r", "--rotation", help="set rotation (0, 90, 180 or 270)", dest="rotation", type=int, default=0)
+    parser.add_argument("-p", "--padding", help="set margin (1 = half block width)", dest="padding", type=int, default=1)
+    parser.add_argument("-b", "--blocksize", help="individual block size in px (default is 5)", dest="blocksize", type=int, default=1)
+    parser.add_argument("-d", "--darken", help="amount to darken each block face (default is 0.05)", dest="darken", type=int, default=0.05)
+    args, extra = parser.parse_known_args()
+
+    bs = args.blocksize
+    padding = args.padding
+    BRIGHTNESS_STEP = args.darken
+    if not args.outputfile:
+        outfile = os.path.splitext(args.inputfile)[0] + '.png'
+    else:
+        outfile = args.outputfile
 
     # read schematics file and determine attributes
-    nbtfile = nbt.NBTFile(INPUT_FILE,'rb')
+    nbtfile = nbt.NBTFile(args.inputfile,'rb')
     height = nbtfile['Height'].value
     length = nbtfile['Length'].value
     width = nbtfile['Width'].value
     blocks = nbtfile['Blocks']
     max_x, max_y, max_z = (width, height, length)
+    w = max_z*bs*2 + max_x*bs*2 + padding*4*bs
+    # FIXME: reliably get proper height from the y value
+    h = max_z*bs + max_x*bs + 120
+    data = nbtfile['Data']
+
 
     # set up Shoebot
-    w = max_z*bs*2 + max_x*bs*2 + padding*4*bs
-    # FIXME: reliably get proper height
-    h = max_z*bs + max_x*bs + 120
+    bot = shoebot.sbot.init_bot(outputfile=outfile) 
+    bot.colormode(bot.RGB)
+    bot.colorrange(255)
     bot.size(w,h) 
     bot.background(255, 255, 255)
-    # start from 40, 40
-    # FIXME: following line exposes a Shoebot bug
-    # bot.translate(40, 40)
-    
     # determine origin point
     origin_x = padding*2*bs + max_z*bs*2
     origin_y = 120
@@ -116,18 +117,27 @@ if __name__ == '__main__':
     # also make a list of block types
     TYPES = []
     matrix = np.empty((max_x+1, max_y+1, max_z+1))
+    datamatrix = np.empty((max_x+1, max_y+1, max_z+1))
+    i = 0
     for x in range(width):
         for y in range(height):    
             for z in range(length):
                 blocktype = get_block_from_nbt(x, y, z, blocks)
+                blockdata = data[i]
                 matrix[x,y,z] = blocktype
+                datamatrix[x,y,z] = blockdata
                 TYPES.append(blocktype)
+                i += 1
     TYPES.sort()
-    print set(TYPES)
+    blocktypes = set(TYPES)
+    for t in blocktypes:
+        if t and t not in BLOCKDEFS:
+            print 'Unsupported block: %d' % t
+    
         
     # apply rotation
     # FIXME: This does not work :(
-    if rotation:
+    if args.rotation:
         from rotation import rotation_matrix
         from math import pi
         axis = [0,1,0]
@@ -142,11 +152,14 @@ if __name__ == '__main__':
                 try:
                     blocktype = matrix[x,y,z]
                 except IndexError:
+                    i += 1
                     continue
                 if blocktype:
+                    blockdata = datamatrix[x,y,z]
                     px = origin_x + (x-z) * (2*bs)
                     pz = origin_y + (x+z) * bs - y*bs*2
-                    draw_block(px, pz, blocktype)    
+                    draw_block(px, pz, blocktype, blockdata, bot)
+                i += 1
     
     bot.finish()
                     
